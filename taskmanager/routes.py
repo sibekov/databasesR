d9948bf59a2a9f8d3aa06328f51414a26a58c833
sibekov <vusimuzij.sibeko@gmail.com>
2024-12-17 22:22:18 +0000

    Add defencive design

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -28,7 +28,7 @@ def edit_category(category_id):
         return redirect(url_for("categories"))
     return render_template("edit_category.html",category=category)
 
-@app.rout("/delete_category/<int:category_id>")
+@app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
