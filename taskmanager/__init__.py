b72cd399ddf3fd19e88acfc47eaae09d12d7df17
sibekov <vusimuzij.sibeko@gmail.com>
2024-12-17 21:00:55 +0000

    Add Creat and read functionality

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,8 +7,9 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"]=os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///taskmanager.db" # os.environ.get("DB_URL")      
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False                          
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False                          
 db = SQLAlchemy(app)
+
 
 
 
